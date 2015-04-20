98beb5776e8611fa0c1c6301488a92c42e90bf09
lAionl <thesinnersoul@gmail.com>
2015-04-20 14:37:45 +0500

    Обновлен токен в vkmusic.py, изменена функция getMusic(), теперь сохраняется файл под своим именем в playlist vk.vom; Добавленны обработки исключительных ситуаций в функциях vkConnect(), getDirPath().

--- vkmusic/vk_downloader.py
+++ vkmusic/vk_downloader.py
@@ -2,9 +2,11 @@
 #-*- coding:utf-8 -*-
 
 """Загрузка музыкальных композиций из vk.com"""
+# token & user_id можно получить пройдя по ссылке:
+# http://api.vkontakte.ru/oauth/authorize?client_id=2223684&scope=audio&redirect_uri=http://api.vk.com/blank.html&display=page&response_type=token
 
 from vkmusic import *
-token = '480c0474a5e0fa863afe2e49ff3825aa6ff04c9fea4056d258256da79e16fe75664d0a6b98f3523f55092'
+token = '13c85c54a174c6a994bf63523518fce77fdb34e9e117543f8878e714ba2918d88e8ba7d7a35794669efc1'
 user_id = '151629380'
 
 
--- vkmusic/vkmusic.py
+++ vkmusic/vkmusic.py
@@ -7,17 +7,6 @@
 # выполните pip install cssselect
 import socket, os, lxml.html, re, urllib.request
 
-# Функция для задания вопросов
-def getquestion(question):
-	answer = input(question + ' (Y/N)')
-	if(answer == 'Y'):
-		return True
-	elif(answer == 'N'):
-		return False
-	else:
-		print('Некорректный ввод попробуйте еще раз.')
-		getquestion(question)
-
 # Функция проверки соединения с сетью
 def checkInet():
 	try:
@@ -29,7 +18,11 @@ def checkInet():
 # Функция подключения к vk и получения списка музыки
 def vkConnect(access_token, user_id):
 	url = "https://api.vkontakte.ru/method/audio.get.xml?uid=" + user_id + "&access_token=" + access_token
-	page = urllib.request.urlopen(url)
+	try:
+		page = urllib.request.urlopen(url)
+	except urllib.error.URLError as e:
+		print(e + '\nПроверьте правильность token для ' + user_id + ',\nтекущее значение = ' + access_token)
+		os.exit()
 	html = page.read()
 	doc = lxml.html.document_fromstring(html)
 	return doc
@@ -43,12 +36,15 @@ def getTrackInfo(target_info, input_doc):
 
 # Функция получения директории под музыку
 def getDirPath():
-	path = 'vkmusic'
-	if os.path.exists(path):
-		return path
-	else:
-		os.makedirs(path)
-		return path
+	path = '~/Downloads/vkmusic'
+	if not os.path.exists(path):
+		try:
+			os.makedirs(path)
+			return path
+		except:
+			print('Не удалось создать дерикторию ' + path)
+			os.exit()
+	return path
 
 # Функция получения исполнителей, названий, ссылок
 def getMusic(doc):
@@ -60,17 +56,9 @@ def getMusic(doc):
 	urlMas = getTrackInfo('url', doc)
 	path = getDirPath()
 	for i in range(len(artistMas)):
-		filename_new = path+"/"+artistMas[i]+" - "+titleMas[i]+".mp3"
+		filename_new = path + "/" + artistMas[i].replace(' ','\ ') + "-" + titleMas[i].replace(' ','\ ') + ".mp3"
 		if os.path.exists(filename_new):
 			print(filename_new + 'уже загружен')
 		else:
-			downCmd = "wget -P " + path + " " + urlMas[i]
+			downCmd = "wget " + urlMas[i] + " -O " + filename_new
 			os.popen(downCmd)
-			
-			p = re.compile(r'[0-9a-zA-Z]+\.mp3$')
-			filename = p.findall(urlMas[i])
-			
-			try:
-				os.rename(path+'/'+filename[0],path+'/'+artistMas[i]+' - '+titleMas[i]+'.mp3')
-			except:
-				print('Невозможно переименовать.')
