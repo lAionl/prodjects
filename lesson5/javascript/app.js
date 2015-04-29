5a9c2911d028fc1ff7341d8be82563705ea964a3
lAionl <thesinnersoul@gmail.com>
2015-04-29 15:17:40 +0500

    -добавление массива объектов hand.json; -изменение содержимого app.js для использования объектов json; -исправление ошибки (была пропущенна запятая) в aceOfSpades.json.

--- lesson5/javascript/app.js
+++ lesson5/javascript/app.js
@@ -1,5 +1,26 @@
 var main = function(){
 	"use strict";
-	console.log("Hello, World");
-}
+	$.getJSON("cards/aceOfSpades.json", function (card){
+		//создаем элемент для хранения карты
+		var $cardParagraph = $("<p>");
+		//создаем
+		$cardParagraph.text(card.rank + " of " + card.suit);
+		//связываем абзац с картой main
+		$("main").append($cardParagraph);	
+	});
+	$.getJSON("cards/hand.json", function (hand){
+		var $list = $("<ul>");
+		//hand - массив, поэтому можно применить к нему интерационный процесс
+		//с помощью цыкла forEach
+		hand.forEach(function (card){
+			//создаем элемент списка для хранения карты
+			//и присоединяем его к списку
+			var $card = $("<li>");
+			$card.text(card.rank + " of " + card.suit);
+			$list.append($card);
+		});
+		//присоединяем список к элементу main
+		$("main").append($list);
+	});
+};
 $(document).ready(main);
