ba1923b7b88c953839694494fcac26b8999bef1a
lAionl <thesinnersoul@gmail.com>
2015-05-11 11:50:32 +0500

    change comments

--- vkmusic/vk_downloader.py
+++ vkmusic/vk_downloader.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 #-*- coding:utf-8 -*-
 
-"""Загрузка музыкальных композиций из vk.com"""
-# token & user_id можно получить пройдя по ссылке:
-# http://api.vkontakte.ru/oauth/authorize?client_id=2223684&scope=audio&redirect_uri=http://api.vk.com/blank.html&display=page&response_type=token
+"""Загрузка музыкальных композиций из vk.com
+ token & user_id можно получить пройдя по ссылке:
+ http://api.vkontakte.ru/oauth/authorize?client_id=2223684&scope=audio&redirect_uri=http://api.vk.com/blank.html&display=page&response_type=token
+"""
 
 from vkmusic import *
-token = '61a4045f0ddd770e3675639bd8cd2c6a4f8f96825cf4e0161abe6d99fd78389d63da2f191514e8c1722a8'
+token = 'b1760b3be39894c16cf2b7181b2397b9a416db0e388f8fa1c80594d9261c2bad137ba6d7a9ecbd08a4434'
 user_id = '151629380'
 
 
@@ -17,4 +18,6 @@ def main():
 		sys.exit
 	my_doc = vk_connect(token,user_id)	
 	get_music(my_doc)
-main()
+
+if __name__ == "__main__":
+    sys.exit(main())
